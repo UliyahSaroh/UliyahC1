ffb79940bb191b51334dac56cec5ab0157d90d9e
Uliyah Saroh <uliyah.saroh@gmail.com>
2016-09-26 19:25:33 +0700

    Add files via upload

---
+++ UliyahSaroh32.ino
@@ -0,0 +1,15 @@
+//Uliyah Saroh
+//XI TKJ 1
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(11,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(11,HIGH);//Pada pin 11, LED menyala
+  delay(3000)
+  digitalWrite(11,LOW);//Pada pin 11, LED mati
+  delay(3000);//Tunda 3 sec pada saat mati
+}
